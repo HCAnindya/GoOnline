26af8ceef9943f7cf033b86f29dd6ac4daa53ecb
anindya <anindya.hajra.choudhuri@hsbc.com.hk>
2020-09-15 23:08:03 +0800

    Customer Management

---
+++ GoOnlineCustomerManagement/src/test/java/com/goonline/customer/management/GoOnlineCustomerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goonline.customer.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoOnlineCustomerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- GoOnlineDiscoveryService/src/main/java/com/go/online/WebSecurityDiscoveryService.java
+++ GoOnlineDiscoveryService/src/main/java/com/go/online/WebSecurityDiscoveryService.java
@@ -1,10 +1,12 @@
 package com.go.online;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
 public class WebSecurityDiscoveryService extends WebSecurityConfigurerAdapter{
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		 http
@@ -15,5 +17,4 @@ public class WebSecurityDiscoveryService extends WebSecurityConfigurerAdapter{
 		 .authenticated()
 		 .and().httpBasic();
 	}
-
 }

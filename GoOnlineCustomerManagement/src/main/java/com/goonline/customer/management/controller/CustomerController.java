9b1804384e3aa637cc9d95972b7b2c9642e1e713
anindya <anindya.hajra.choudhuri@hsbc.com.hk>
2020-09-16 00:03:38 +0800

    Rest API

---
+++ GoOnlineCustomerManagement/src/main/java/com/goonline/customer/management/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package com.goonline.customer.management.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+	@GetMapping("/status")
+	public String status() {
+		return "All Ok";
+	}
+}

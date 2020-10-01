36cc52b9ee98286e3dcaccad5fd7cd1cf7717352
anindya <anindya.choudhuri.uk@gmail.com>
2020-10-01 08:09:13 +0800

    GoOnlineZuul

---
+++ GoOnlineZuul/src/test/java/com/goonline/zuul/GoOnlineZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goonline.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoOnlineZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
